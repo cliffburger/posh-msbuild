be85f2c2aa40682af3edf201f709780188b8e8ce
Cliff Burger <cliff.burger@gmail.com>
2013-07-31 14:40:08 -0700

    Sample profile

---
+++ msbuild.profile.ps1
@@ -0,0 +1,10 @@
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+# Load posh-git module from current directory
+import-module .\posh-msbuild.psm1
+
+# If module is installed in a default location ($env:PSModulePath),
+# use this instead (see about_Modules for more information):
+# Import-Module posh-git
+Pop-Location
+ 
